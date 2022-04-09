2f93f645eb4a534a5006a16184e3c3c64098c02f
lswatson16 <lauralynw16@gmail.com>
2022-04-09 16:43:16 -0400

    Create controlled form

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -2,9 +2,26 @@ import {Component} from 'react';
 import './Form.css';
 
 class Form extends Component {
+  state = {
+    ingredient1: ''
+  }
+
+  updateForm = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
     return(
-      <form></form>
+      <form>
+        <h2>What ingredients would you like to use?</h2>
+        <input
+          type='text'
+          placeholder='example: chicken'
+          value={this.state.ingredient1}
+          name='ingredient1'
+          onChange={(e)=> this.updateForm(e)}
+        />
+      </form>
     )
   }
 }
