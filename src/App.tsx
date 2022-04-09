acaf53e9694f9fece55c2abd98029027bb76f8bb
lswatson16 <lauralynw16@gmail.com>
2022-04-09 18:03:27 -0400

    Add handler function and pass method as a prop to form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Component} from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Form from './Components/Form/Form';
+import apiCalls from './apiCalls';
 
 class App extends Component {
   state = {
@@ -9,12 +10,16 @@ class App extends Component {
     error: null
   }
 
+  searchSubmit = (ingredient: string) => {
+    apiCalls.searchRecipes(ingredient).then(data => this.setState({ recipes: data.hits }))
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <main>
-          <Form />
+          <Form searchSubmit={this.searchSubmit} />
         </main>
       </div>
     );
