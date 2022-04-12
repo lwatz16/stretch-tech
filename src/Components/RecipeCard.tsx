84c3e7f5c6f0c6fb5d3894401c558fc28c5d8140
Rory Magee <roryemagee@gmail.com>
2022-04-12 11:53:16 -0600

    Add grid properties

--- src/Components/RecipeCard.tsx
+++ src/Components/RecipeCard.tsx
@@ -6,11 +6,15 @@ interface RecipeCardProps extends RecipeInterface {
 
 const RecipeCard = (props: RecipeCardProps) => {
   return (
-    <article>
+    <article className="recipe-card">
+      <div className="recipe-writing">
+        <div className="recipe-details">
+          <h3>{props.label}</h3>
+          <p>{props.calories.toFixed(0)} cal</p>
+        </div>
+        <button>See Details</button>
+      </div>
       <img src={props.images.REGULAR.url} />
-      <h3>{props.label}</h3>
-      <p>{props.calories.toFixed(0)} cal</p>
-      <button>See Details</button>
     </article>
   )
 }
