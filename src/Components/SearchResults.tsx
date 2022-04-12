b4bc1cbc2cd69d9a58773a1325c0bc72bf0c170a
Kim Ward <kmewrd@outlook.com>
2022-04-12 13:02:08 -0500

    Adjust indentations

--- src/Components/SearchResults.tsx
+++ src/Components/SearchResults.tsx
@@ -7,20 +7,23 @@ interface SearchResults {
 
 const SearchResults = ({recipes}: SearchResults) => {
   const recipeCards = recipes.map((recipe, index) => {
-    return (<RecipeCard 
-              key={index.toString()}
-              uri={recipe.uri}
-              label={recipe.label}
-              images={recipe.images}
-              url={recipe.url}
-              yield={recipe.yield}
-              dietLabels={recipe.dietLabels}
-              healthLabels={recipe.healthLabels}
-              calories={recipe.calories}
-              mealType={recipe.mealType}
-              cuisineType={recipe.cuisineType}
-            />)
+    return (
+      <RecipeCard 
+        key={index.toString()}
+        uri={recipe.uri}
+        label={recipe.label}
+        images={recipe.images}
+        url={recipe.url}
+        yield={recipe.yield}
+        dietLabels={recipe.dietLabels}
+        healthLabels={recipe.healthLabels}
+        calories={recipe.calories}
+        mealType={recipe.mealType}
+        cuisineType={recipe.cuisineType}
+      />
+      )
   });
+  
   return (
     <section className="search-results">{recipeCards}</section>
   )
