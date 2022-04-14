585d7d1474d31427e9ba95d3abb0ff09add133d0
Rory Magee <roryemagee@gmail.com>
2022-04-14 16:22:45 -0600

    Add rough outline of FilterMenu class

---
+++ src/Components/FilterMenu.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import { isPropertySignature } from 'typescript';
+
+// interface FilterMenuProps {
+
+// }
+
+// interface FilterMenuState {
+//   filter: string
+// }
+
+// class FilterMenu extends Component<FilterMenuProps, FilterMenuState> {
+//   constructor(props) {
+//     super(props)
+//     this.state {
+//       filter: ''
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <select>
+        
+//       </select>
+//     )
+//   }
+// }
+
+// export default FilterMenu;
