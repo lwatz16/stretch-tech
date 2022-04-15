e25db886f9f3adf3bc9506b1cecf9934a009ab79
Dillon Parker <prkr93@gmail.com>
2022-04-15 15:50:42 -0600

    Add error handling for request failure for single recipe

--- src/Components/SingleRecipe.tsx
+++ src/Components/SingleRecipe.tsx
@@ -8,7 +8,8 @@ interface SingleRecipeProps {
 }
 
 interface SingleRecipeState extends RecipeInterface {
-  ingredientLines: string[]
+  ingredientLines: string[],
+  error: string
 }
 
 class SingleRecipe extends Component<SingleRecipeProps, SingleRecipeState> {
@@ -29,14 +30,15 @@ class SingleRecipe extends Component<SingleRecipeProps, SingleRecipeState> {
       calories: 0,
       mealType: [],
       cuisineType: [],
-      ingredientLines: []
+      ingredientLines: [],
+      error: ''
     }
   }
 
   getRecipe = (uri: string) => {
     apiCalls.fetchSingleRecipe(uri)
-      .then(data => this.setState({ uri: data.recipe.uri, label: data.recipe.label, url: data.recipe.url, yield: data.recipe.yield, dietLabels: data.recipe.dietLabels, healthLabels: data.recipe.healthLabels, calories: data.recipe.calories, mealType: data.recipe.mealType, cuisineType: data.recipe.cuisineType, images: {REGULAR: {url: data.recipe.images.REGULAR.url}}, ingredientLines: data.recipe.ingredientLines }))
-      .catch(err => console.log(err));
+      .then(data => this.setState({ uri: data.recipe.uri, label: data.recipe.label, url: data.recipe.url, yield: data.recipe.yield, dietLabels: data.recipe.dietLabels, healthLabels: data.recipe.healthLabels, calories: data.recipe.calories, mealType: data.recipe.mealType, cuisineType: data.recipe.cuisineType, images: {REGULAR: {url: data.recipe.images.REGULAR.url}}, ingredientLines: data.recipe.ingredientLines, error: '' }))
+      .catch(err => this.setState({ error: `Something went wrong, please try again later. ${err}.` }));
   }
 
   componentDidMount = () => this.getRecipe(this.props.uri)
@@ -51,17 +53,22 @@ class SingleRecipe extends Component<SingleRecipeProps, SingleRecipeState> {
     return (
       <section className='single-recipe-wrapper'>
         <button type='button' onClick={this.props.backToSearchResults}>Back to Results</button>
-        <img src={this.state.images.REGULAR.url} alt={this.state.label}/>
-        <h2>{this.state.label}</h2>
-        <p>{this.state.yield} servings</p>
-        <p>{this.state.calories.toFixed(2)} cal</p>
-        <p>{dietLabels}</p>
-        <p>{healthLabels}</p>
-        <p>{mealType}</p>
-        <p>{cuisineType}</p>
-        <p>Ingredients: </p>
-        {ingredientsList}
-        <a href={this.state.url} target='_blank'><button>See Full Recipe</button></a>
+        <div className='error'>{this.state.error}</div>
+        {!!this.state.uri.length && (
+          <div>
+            <img src={this.state.images.REGULAR.url} alt={this.state.label} />
+            <h2>{this.state.label}</h2>
+            <p>{this.state.yield} servings</p>
+            <p>{this.state.calories.toFixed(2)} cal</p>
+            <p>{dietLabels}</p>
+            <p>{healthLabels}</p>
+            <p>{mealType}</p>
+            <p>{cuisineType}</p>
+            <p>Ingredients: </p>
+            {ingredientsList}
+            <a href={this.state.url} target='_blank'><button>See Full Recipe</button></a>
+          </div>
+        )}
       </section>
     )
   }
