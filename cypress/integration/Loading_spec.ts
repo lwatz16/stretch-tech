6d9bc76bd6b572cec208d09413c61d54b8b187e2
lswatson16 <lauralynw16@gmail.com>
2022-04-17 13:28:54 -0400

    Revise testing for the loading component

--- cypress/integration/Loading_spec.ts
+++ cypress/integration/Loading_spec.ts
@@ -15,7 +15,7 @@ describe('Loading', () => {
     }).as('getSingleRecipe')
   })
   
-  it('should show a loading component when I click Find Recipes and View button', () => {
+  it('should show a loading component when I click the Find Recipes button and View button on the HomePage', () => {
     cy.get('input')
       .type('chicken')
 
@@ -35,10 +35,47 @@ describe('Loading', () => {
       .contains('View')
       .click()
 
+    cy.wait('@getSingleRecipe').then(interception => {
+      const response = interception.response.body.recipe;
+      cy.get('.loading')
+        .should('be.visible')
+      expect(response.calories).to.eq(184.9062325245606)
+    })
+
+    cy.url()
+      .should('eq', Cypress.env('url') + 'recipe/be262659c04aed267fd34c2b0606ed6e')
+  })
+
+  it('should show a loading component when I click Back to Results button', () => {
+    cy.get('input')
+      .type('chicken')
+
+    cy.get('.add-input-btn')
+      .click()
+
+    cy.get('.search-btn')
+      .contains('Find Recipes')
+      .click()
+
+    cy.get('.recipe-card')
+      .first()
+      .find('button')
+      .contains('View')
+      .click()
+    
     cy.url()
       .should('eq', Cypress.env('url') + 'recipe/be262659c04aed267fd34c2b0606ed6e')
 
-    cy.get('.loading')
-      .should('be.visible')
+    cy.get('button')
+      .contains('Back to Results')
+      .click()
+
+    cy.wait('@getRecipes').then(() => {
+      cy.get('.loading')
+        .should('be.visible')
+    })
+
+    cy.url()
+      .should('eq', Cypress.env('url') + 'ingredients/chicken')
   })
 })
