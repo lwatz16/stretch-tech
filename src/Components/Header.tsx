8773e7e756d8f8db2040191d66868dda0dad35c0
Dillon Parker <prkr93@gmail.com>
2022-04-18 14:23:54 -0600

    Add logo for favicon/header, style for header, and add header link to homepage

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
-      <h1>Recip-Eco</h1>
+      <Link to='/'>
+        <img src='/recipeco.png' alt='Recip-Eco logo' />
+        <h1>Recip-Eco</h1>
+      </Link>
     </header>
   )
 }
